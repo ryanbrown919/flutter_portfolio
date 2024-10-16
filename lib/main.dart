eff7ac79c39fca852c947c110155535e5536e8f9
Ryan Brown <ryanbrown919@gmail.com>
2024-10-15 22:57:45 -0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyPortfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ryan Brown Portfolio',
+      title: "Ryan Brown's Portfolio",
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
