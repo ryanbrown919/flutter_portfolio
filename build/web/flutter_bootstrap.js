4430fb0219eefdefc7f3fa6a16a726013af3c880
Ryan Brown <ryanbrown919@gmail.com>
2024-10-15 23:08:30 -0700

    test

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2974193732"
+    serviceWorkerVersion: "3802576876"
   }
 });
